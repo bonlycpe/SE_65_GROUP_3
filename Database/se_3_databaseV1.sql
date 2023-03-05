7b35faa04cb0d1592c78b691eeadbd8d4aa40ff0
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2023-03-05 15:05:37 +0700

    upload Database

---
+++ Database/se_3_databaseV1.sql
@@ -0,0 +1,401 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 05, 2023 at 09:02 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `se_3_database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign`
+--
+
+CREATE TABLE `campaign` (
+  `campaign_id` int(11) NOT NULL,
+  `campaign_name` varchar(50) NOT NULL,
+  `campaign_description` varchar(255) NOT NULL,
+  `campaign_status` enum('ACTIVE','FINISHED','TERMINATE') NOT NULL DEFAULT 'ACTIVE'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_money`
+--
+
+CREATE TABLE `campaign_money` (
+  `campaign_money_id` int(11) NOT NULL,
+  `campaign_money_goal` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_object`
+--
+
+CREATE TABLE `campaign_object` (
+  `campaign_object_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_object_request`
+--
+
+CREATE TABLE `campaign_object_request` (
+  `campaign_object_request_id` int(11) NOT NULL,
+  `campaign_object_request_amount` float NOT NULL,
+  `campaign_object_date` date NOT NULL DEFAULT current_timestamp(),
+  `campaign_object_vote_statuste` enum('REQUEST','APPROVE','DENY') NOT NULL DEFAULT 'REQUEST',
+  `user_id` int(11) NOT NULL,
+  `campaign_object_id` int(11) NOT NULL,
+  `campaign_project_user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_project_user`
+--
+
+CREATE TABLE `campaign_project_user` (
+  `campaign_project_user_id` int(11) NOT NULL,
+  `campaign_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `campaign_project_user_type` enum('CHAIRMAN','BOARD') NOT NULL DEFAULT 'BOARD'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `campaign_user_donate`
+--
+
+CREATE TABLE `campaign_user_donate` (
+  `campaign_user_donate_id` int(11) NOT NULL,
+  `campaign_user_donate_img_name` varchar(50) NOT NULL,
+  `campaign_user_donate_amount` float NOT NULL,
+  `campaign_user_donate_approve_status` enum('REQUEST','APPROVE','DENY') NOT NULL DEFAULT 'REQUEST',
+  `campaign_money_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `object`
+--
+
+CREATE TABLE `object` (
+  `object_id` int(11) NOT NULL,
+  `object_name` varchar(50) NOT NULL,
+  `object_img_name` varchar(50) NOT NULL,
+  `object_amount` int(11) NOT NULL,
+  `campaign_object_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `progress`
+--
+
+CREATE TABLE `progress` (
+  `progress_id` int(11) NOT NULL,
+  `progress_number` int(11) NOT NULL,
+  `progress_name` varchar(50) NOT NULL,
+  `progress_description` varchar(255) NOT NULL,
+  `campaign_money_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `staff_id` int(11) NOT NULL,
+  `staff_name` varchar(50) NOT NULL,
+  `staff_email` varchar(50) NOT NULL,
+  `staff_password` varchar(255) NOT NULL,
+  `staff_type` enum('MONEY','VERIFY') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag_campaign_object`
+--
+
+CREATE TABLE `tag_campaign_object` (
+  `tag_campaign_object_id` int(11) NOT NULL,
+  `tag_campaign_object_name` varchar(50) NOT NULL,
+  `campaign_object_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tag_user`
+--
+
+CREATE TABLE `tag_user` (
+  `tag_user_id` int(11) NOT NULL,
+  `tag_user_name` varchar(50) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_firstname` varchar(50) NOT NULL,
+  `user_lastname` varchar(50) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `user_password` varchar(255) NOT NULL,
+  `user_permission` enum('NULL','REQUEST','APPROVE') NOT NULL DEFAULT 'NULL'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `campaign`
+--
+ALTER TABLE `campaign`
+  ADD PRIMARY KEY (`campaign_id`);
+
+--
+-- Indexes for table `campaign_money`
+--
+ALTER TABLE `campaign_money`
+  ADD PRIMARY KEY (`campaign_money_id`),
+  ADD KEY `campaign_money_id` (`campaign_money_id`);
+
+--
+-- Indexes for table `campaign_object`
+--
+ALTER TABLE `campaign_object`
+  ADD PRIMARY KEY (`campaign_object_id`),
+  ADD KEY `campaign_object_id` (`campaign_object_id`);
+
+--
+-- Indexes for table `campaign_object_request`
+--
+ALTER TABLE `campaign_object_request`
+  ADD PRIMARY KEY (`campaign_object_request_id`),
+  ADD KEY `user_id` (`user_id`,`campaign_object_id`,`campaign_project_user_id`),
+  ADD KEY `campaign_object_id` (`campaign_object_id`),
+  ADD KEY `campaign_project_user_id` (`campaign_project_user_id`);
+
+--
+-- Indexes for table `campaign_project_user`
+--
+ALTER TABLE `campaign_project_user`
+  ADD PRIMARY KEY (`campaign_project_user_id`),
+  ADD KEY `campaign_id` (`campaign_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `campaign_user_donate`
+--
+ALTER TABLE `campaign_user_donate`
+  ADD PRIMARY KEY (`campaign_user_donate_id`),
+  ADD KEY `campaign_money_id` (`campaign_money_id`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `object`
+--
+ALTER TABLE `object`
+  ADD PRIMARY KEY (`object_id`),
+  ADD KEY `campaign_object_id` (`campaign_object_id`);
+
+--
+-- Indexes for table `progress`
+--
+ALTER TABLE `progress`
+  ADD PRIMARY KEY (`progress_id`),
+  ADD KEY `campaign_money_id` (`campaign_money_id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`staff_id`);
+
+--
+-- Indexes for table `tag_campaign_object`
+--
+ALTER TABLE `tag_campaign_object`
+  ADD PRIMARY KEY (`tag_campaign_object_id`),
+  ADD KEY `campaign_object_id` (`campaign_object_id`);
+
+--
+-- Indexes for table `tag_user`
+--
+ALTER TABLE `tag_user`
+  ADD PRIMARY KEY (`tag_user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `campaign`
+--
+ALTER TABLE `campaign`
+  MODIFY `campaign_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `campaign_object_request`
+--
+ALTER TABLE `campaign_object_request`
+  MODIFY `campaign_object_request_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `campaign_project_user`
+--
+ALTER TABLE `campaign_project_user`
+  MODIFY `campaign_project_user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `campaign_user_donate`
+--
+ALTER TABLE `campaign_user_donate`
+  MODIFY `campaign_user_donate_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `object`
+--
+ALTER TABLE `object`
+  MODIFY `object_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `progress`
+--
+ALTER TABLE `progress`
+  MODIFY `progress_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tag_campaign_object`
+--
+ALTER TABLE `tag_campaign_object`
+  MODIFY `tag_campaign_object_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tag_user`
+--
+ALTER TABLE `tag_user`
+  MODIFY `tag_user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `campaign_money`
+--
+ALTER TABLE `campaign_money`
+  ADD CONSTRAINT `campaign_money_ibfk_1` FOREIGN KEY (`campaign_money_id`) REFERENCES `campaign` (`campaign_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `campaign_object`
+--
+ALTER TABLE `campaign_object`
+  ADD CONSTRAINT `campaign_object_ibfk_1` FOREIGN KEY (`campaign_object_id`) REFERENCES `campaign` (`campaign_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `campaign_object_request`
+--
+ALTER TABLE `campaign_object_request`
+  ADD CONSTRAINT `campaign_object_request_ibfk_1` FOREIGN KEY (`campaign_object_id`) REFERENCES `campaign_object` (`campaign_object_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `campaign_object_request_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `campaign_object_request_ibfk_3` FOREIGN KEY (`campaign_project_user_id`) REFERENCES `campaign_project_user` (`campaign_project_user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `campaign_project_user`
+--
+ALTER TABLE `campaign_project_user`
+  ADD CONSTRAINT `campaign_project_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `campaign_project_user_ibfk_2` FOREIGN KEY (`campaign_id`) REFERENCES `campaign` (`campaign_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `campaign_user_donate`
+--
+ALTER TABLE `campaign_user_donate`
+  ADD CONSTRAINT `campaign_user_donate_ibfk_1` FOREIGN KEY (`campaign_money_id`) REFERENCES `campaign_money` (`campaign_money_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `campaign_user_donate_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `object`
+--
+ALTER TABLE `object`
+  ADD CONSTRAINT `object_ibfk_1` FOREIGN KEY (`campaign_object_id`) REFERENCES `campaign_object` (`campaign_object_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `progress`
+--
+ALTER TABLE `progress`
+  ADD CONSTRAINT `progress_ibfk_1` FOREIGN KEY (`campaign_money_id`) REFERENCES `campaign_money` (`campaign_money_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tag_campaign_object`
+--
+ALTER TABLE `tag_campaign_object`
+  ADD CONSTRAINT `tag_campaign_object_ibfk_1` FOREIGN KEY (`campaign_object_id`) REFERENCES `campaign_object` (`campaign_object_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tag_user`
+--
+ALTER TABLE `tag_user`
+  ADD CONSTRAINT `tag_user_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

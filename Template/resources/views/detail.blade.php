d60af9d898fb76e04994c3ba4f978e26bb7c9f30
NPJA <123374602+NPJA@users.noreply.github.com>
2023-03-22 12:43:52 +0700

    Add files via upload

---
+++ Template/resources/views/detail.blade.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device=width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/981942107730243604/1083608810524004432/nt.png">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"
+        rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link href="css/templatemo-pod-talk.css" rel="stylesheet">
+</head>
+</html>
+
+<body>
+
+    <main>
+        
+    @include('topbar')
+
+
+        <section class="trending-podcast-section detail-section-padding ">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="section-title-wrap mb-5">
+                            <h4 class="section-title">Details</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-lg">
+                    <div class="detail">
+                        <div class="detail-block detail-block-full">
+                            <div class="detail-block-image-wrap">
+                                <a href="">
+                                    <img src="https://www.ramafoundation.or.th/give/uploads/projects/thumbnail/6253af47601de.jpg" 
+                                    class="detail-block-image img-fluid"
+                                        alt="">
+                                </a>
+                            </div>
+
+                            <div class="detail-block-info">
+                                <h5 class="mb-2">
+                                    <a href="">
+                                    โครงการศูนย์นวัตกรรมการแพทย์เพื่อผู้สูงวัยและผู้ป่วยระยะท้าย
+                                    </a>
+                                </h5>
+
+                                <div class="profile-block d-flex">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjkBeMINR-D17nA0p-gJocFyUBqSyM82pSSA&usqp=CAU"
+                                        class="profile-block-image img-fluid" alt="">
+
+                                    <p>นาย A
+                                        <strong>ผู้ก่อตั้งโครงการ</strong>
+                                    </p>
+                                </div>
+                                    <p>  ปัจจุบันประเทศไทยเข้าสู่สังคมผู้สูงวัยอย่างสมบูรณ์แล้ว โดยมีผู้สูงวัยมากถึง 12 ล้านคน หรือร้อยละ 20 และในปี พ.ศ.2578 จะเพิ่มเป็นร้อยละ 30 ซึ่งหมายความว่า ทุก ๆ ประชากร 100 คน เราจะพบผู้สูงวัย 30 คน</p>
+                                    <p>  ตัวเลขดังกล่าวชี้ให้เห็นว่าพวกเราทุกคน ทุกครอบครัวกำลังต้องรับมือกับการดูแลผู้สูงวัยที่อยู่ในภาวะที่มีความเสี่ยงในการเจ็บป่วยด้วยโรคต่าง ๆ มากมาย อาทิ โรคเรื้อรัง อย่างเบาหวาน ไขมัน ความดัน ความจำเสื่อม และโรคที่เกิดจากการป่วยระยะท้าย เช่น มะเร็ง ตับวาย ไตวาย หัวใจล้มเหลว รวมถึงภาวะสุขภาพจิตที่จำเป็นต้องดูแลด้วยความรู้ความเข้าใจ</p>
+                                    <p>  ทำความรู้จักและดูแลผู้สูงวัยตามสมรรถภาพร่างกาย...</p>
+                
+                                <ol>
+                                    <li>  
+                                        <p>  กลุ่มผู้สูงวัยที่มีสุขภาวะดี ควรสร้างเสริมความแข็งแรง ชะลอความเสื่อมของร่างกายให้นานที่สุด</p>
+                                    </li>
+                                    <li>  
+                                        <p>  กลุ่มผู้สูงวัยที่มีภาวะพึ่งพิง ต้องฟื้นฟูสุขภาพเพื่อลดการพึ่งพิง ให้ใช้ชีวิตประจำวันได้อย่างไม่ลำบาก</p>
+                                    </li>
+                                    <li>  
+                                        <p>  กลุ่มผู้ป่วยระยะท้าย จำเป็นต้องดูแลทั้งร่างกาย และจิตใจให้ผ่านช่วงเวลาสุดท้ายในชีวิตอย่างมีคุณภาพและมีความสุข</p>
+                                    </li>
+                                </ol>
+                            
+<!-- 
+                                <div class="mt-2">
+                                    <a href="" class="btn custom-btn">
+                                    บริจาคทันที
+                                    </a>
+                                    <a href="" class="btn custom-btn">
+                                    บริจาครายเดือน
+                                    </a>
+                                </div> -->
+                            </div>
+
+                        </div>
+                    </div>    
+                    </div>
+
+                    <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+                        <div class="detail">
+                            <div class="detail-block detail-block-full">
+                                <div class="detail-block-info">
+                                    <h5 class="mb-2">
+                                        <a href="">
+                                        ยอดบริจาคทั้งหมด
+                                        </a>
+                                    </h5>
+
+                                    <div class="profile-block d-flex">
+                                        <p>
+                                            <strong>15,500 บาท</strong>
+                                        </p>
+                                    </div>
+                                
+
+                                    <div class="mt-2">
+                                        <a href="" class="btn custom-btn">
+                                        บริจาค
+                                        </a>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>    
+                    </div>
+
+                    <!-- <div class="detail">
+                        <div class="detail-block detail-block-full">
+                            <div class="detail-block-image-wrap">
+                                <a href="">
+                                    <img src="https://www.ramafoundation.or.th/give/uploads/projects/thumbnail/6253af47601de.jpg" 
+                                    class="detail-block-image img-fluid"
+                                        alt="">
+                                </a>
+                            </div>
+
+                            <div class="detail-block-info">
+                                <h5 class="mb-2">
+                                    <a href="">
+                                    โครงการศูนย์นวัตกรรมการแพทย์เพื่อผู้สูงวัยและผู้ป่วยระยะท้าย
+                                    </a>
+                                </h5>
+
+                                <div class="profile-block d-flex">
+                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjkBeMINR-D17nA0p-gJocFyUBqSyM82pSSA&usqp=CAU"
+                                        class="profile-block-image img-fluid" alt="">
+
+                                    <p>นาย A
+                                        <strong>ผู้ก่อตั้งโครงการ</strong>
+                                    </p>
+                                </div>
+                                    <p>  ปัจจุบันประเทศไทยเข้าสู่สังคมผู้สูงวัยอย่างสมบูรณ์แล้ว โดยมีผู้สูงวัยมากถึง 12 ล้านคน หรือร้อยละ 20 และในปี พ.ศ.2578 จะเพิ่มเป็นร้อยละ 30 ซึ่งหมายความว่า ทุก ๆ ประชากร 100 คน เราจะพบผู้สูงวัย 30 คน</p>
+                                    <p>  ตัวเลขดังกล่าวชี้ให้เห็นว่าพวกเราทุกคน ทุกครอบครัวกำลังต้องรับมือกับการดูแลผู้สูงวัยที่อยู่ในภาวะที่มีความเสี่ยงในการเจ็บป่วยด้วยโรคต่าง ๆ มากมาย อาทิ โรคเรื้อรัง อย่างเบาหวาน ไขมัน ความดัน ความจำเสื่อม และโรคที่เกิดจากการป่วยระยะท้าย เช่น มะเร็ง ตับวาย ไตวาย หัวใจล้มเหลว รวมถึงภาวะสุขภาพจิตที่จำเป็นต้องดูแลด้วยความรู้ความเข้าใจ</p>
+                                    <p>  ทำความรู้จักและดูแลผู้สูงวัยตามสมรรถภาพร่างกาย...</p>
+                
+                                <ol>
+                                    <li>  
+                                        <p>  กลุ่มผู้สูงวัยที่มีสุขภาวะดี ควรสร้างเสริมความแข็งแรง ชะลอความเสื่อมของร่างกายให้นานที่สุด</p>
+                                    </li>
+                                    <li>  
+                                        <p>  กลุ่มผู้สูงวัยที่มีภาวะพึ่งพิง ต้องฟื้นฟูสุขภาพเพื่อลดการพึ่งพิง ให้ใช้ชีวิตประจำวันได้อย่างไม่ลำบาก</p>
+                                    </li>
+                                    <li>  
+                                        <p>  กลุ่มผู้ป่วยระยะท้าย จำเป็นต้องดูแลทั้งร่างกาย และจิตใจให้ผ่านช่วงเวลาสุดท้ายในชีวิตอย่างมีคุณภาพและมีความสุข</p>
+                                    </li>
+                                </ol>
+                            
+
+                                <div class="mt-2">
+                                    <a href="" class="btn custom-btn">
+                                    บริจาคทันที
+                                    </a>
+                                    <a href="" class="btn custom-btn">
+                                    บริจาครายเดือน
+                                    </a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>     -->
+                                   
+                </div>
+            </div>
+        </section>
+       
+    </main>
+    <!-- JAVASCRIPT FILES -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/custom.js"></script>
+
+</body>
+
+</html>

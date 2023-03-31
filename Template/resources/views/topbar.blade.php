d055df5f22149dce5102356d33b1f2ff7c65ef5e
NPJA <123374602+NPJA@users.noreply.github.com>
2023-03-31 16:34:58 +0700

    Add files via upload

--- Template/resources/views/topbar.blade.php
+++ Template/resources/views/topbar.blade.php
@@ -24,10 +24,10 @@
             <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <a class="navbar-brand me-lg-5 me-0" href="index">
-                    <img src="img/nt.jpg" class="logo-image img-fluid" alt="templatemo pod talk">
+                    <img src="https://cdn.discordapp.com/attachments/981942107730243604/1083608810524004432/nt.png" class="logo-image img-fluid" alt="templatemo pod talk">
                 </a>
 
-                <form action="#" method="get" class="custom-form search-form flex-fill me-3" role="search">
+                {{-- <form action="#" method="get" class="custom-form search-form flex-fill me-3" role="search">
                     <div class="input-group input-group-lg">
                         <input name="search" type="search" class="form-control" style="background-color: #000;" id="search" placeholder="Search Campaing"
                             aria-label="Search">
@@ -36,7 +36,7 @@
                             <i class="bi-search"></i>
                         </button>
                     </div>
-                </form>
+                </form> --}}
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -47,22 +47,25 @@
                         <li class="nav-item">
                             <a class="nav-link " href="index">หน้าแรก</a>
                         </li>
-                        <li class="nav-item dropdown">
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="index">เกี่ยวกับเรา</a>
+                        </li> -->
+                        <!-- <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">เกี่ยวกับเรา</a>
+                                data-bs-toggle="dropdown" aria-expanded="false">เกี่ยวกับเรา</a> -->
 
-                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                            <!-- <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
                                 <li><a class="dropdown-item" href="">test</a></li>
 
                                 <li><a class="dropdown-item" href="">test</a></li>
-                            </ul>
-                        </li>
+                            </ul> -->
+                        <!-- </li> -->
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="projectCart" id="navbarLightDropdownMenuLink" role="button"
                                 data-bs-toggle="dropdown" aria-expanded="false">โครงการ</a>
 
                             <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                                <li><a class="dropdown-item" href="projectCart">ALL</a></li>
+                                <li><a class="dropdown-item" href="projectCart">รวมโครงการ</a></li>
 
                             </ul>
                         </li>

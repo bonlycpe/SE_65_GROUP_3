85096f00a84ffb08f4f1a32aee8b8c63b6d90232
NPJA <123374602+NPJA@users.noreply.github.com>
2023-03-31 17:19:24 +0700

    Add files via upload

--- myProject/resources/views/layouts/topbar.blade.php
+++ myProject/resources/views/layouts/topbar.blade.php
@@ -1,88 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Climate+Crisis&display=swap" rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <link rel="shortcut icon"
-        href="https://cdn.discordapp.com/attachments/981942107730243604/1083608810524004432/nt.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/981942107730243604/1083608810524004432/nt.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"
-        rel="stylesheet">
-
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"rel="stylesheet">
+    
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-icons.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link href="css/templatemo-pod-talk.css" rel="stylesheet">
 </head>
-
 <body>
-
+    
 </body>
-
 </html>
 
-<nav class="navbar navbar-expand-lg">
-    <div class="container">
-        <a class="navbar-brand me-lg-5 me-0" href="index">
-            <img src="img/nt.jpg" class="logo-image img-fluid" alt="templatemo pod talk">
-        </a>
+            <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand me-lg-5 me-0" href="index">
+                    <img src="https://cdn.discordapp.com/attachments/981942107730243604/1083608810524004432/nt.png" class="logo-image img-fluid" alt="templatemo pod talk">
+                </a>
 
-        <form action="#" method="get" class="custom-form search-form flex-fill me-3" role="search">
-            <div class="input-group input-group-lg">
-                <input name="search" type="search" class="form-control" style="background-color: #000;" id="search"
-                    placeholder="Search Campaing" aria-label="Search">
+                {{-- <form action="#" method="get" class="custom-form search-form flex-fill me-3" role="search">
+                    <div class="input-group input-group-lg">
+                        <input name="search" type="search" class="form-control" style="background-color: #000;" id="search" placeholder="Search Campaing"
+                            aria-label="Search">
 
-                <button type="submit" class="form-control" id="submit">
-                    <i class="bi-search"></i>
+                        <button type="submit" class="form-control" id="submit">
+                            <i class="bi-search"></i>
+                        </button>
+                    </div>
+                </form> --}}
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
                 </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-auto">
+                        <li class="nav-item">
+                            <a class="nav-link " href="index">หน้าแรก</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="index">เกี่ยวกับเรา</a>
+                        </li> -->
+                        <!-- <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">เกี่ยวกับเรา</a> -->
+
+                            <!-- <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                                <li><a class="dropdown-item" href="">test</a></li>
+
+                                <li><a class="dropdown-item" href="">test</a></li>
+                            </ul> -->
+                        <!-- </li> -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="projectCart" id="navbarLightDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">โครงการ</a>
+
+                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                                <li><a class="dropdown-item" href="projectCart">รวมโครงการ</a></li>
+
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index">ข่าวสาร</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contract">ติดต่อ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login">ออกจากระบบ</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </form>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        </nav>
+        
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-lg-auto">
-                <li class="nav-item">
-                    <a class="nav-link " href="index">หน้าแรก</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarLightDropdownMenuLink" role="button"
-                        data-bs-toggle="dropdown" aria-expanded="false">เกี่ยวกับเรา</a>
-
-                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                        <li><a class="dropdown-item" href="">test</a></li>
-
-                        <li><a class="dropdown-item" href="">test</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="projectCart" id="navbarLightDropdownMenuLink"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">โครงการ</a>
-
-                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                        <li><a class="dropdown-item" href="projectCart">ALL</a></li>
-
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index">ข่าวสาร</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contract">ติดต่อ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="logout">ออกจากระบบ</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>

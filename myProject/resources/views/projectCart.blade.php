8f92b7cfffeac8b8a01a1bcc10bde1a110eae76f
NPJA <123374602+NPJA@users.noreply.github.com>
2023-03-31 17:17:53 +0700

    Add files via upload

--- myProject/resources/views/projectCart.blade.php
+++ myProject/resources/views/projectCart.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device=width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Project</title>
     <!-- <link rel="stylesheet" href="css/style.css"> -->
-    <link rel="shortcut icon" href="img/nt.jpg">
+    <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/981942107730243604/1083608810524004432/nt.png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
 
@@ -16,6 +15,8 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"
         rel="stylesheet">
+    
+    <link rel="stylesheet" href="css/style.css">
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
@@ -27,14 +28,59 @@
 
     <link href="css/templatemo-pod-talk.css" rel="stylesheet">
 </head>
-
 </html>
 
 <body>
 
     <main>
-        @include('layouts/topbar')
+        
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand me-lg-5 me-0" href="index">
+                    <img src="https://cdn.discordapp.com/attachments/981942107730243604/1083608810524004432/nt.png" class="logo-image img-fluid" alt="templatemo pod talk">
+                </a>
+                <form action="#" method="get" class="custom-form search-form flex-fill me-3" role="search">
+                    <div class="input-group input-group-lg">
+                        <input name="search" type="search" class="form-control" style="background-color: #000;" id="search" placeholder="Search Campaing"
+                            aria-label="Search">
 
+                        <button type="submit" class="form-control" id="submit">
+                            <i class="bi-search"></i>
+                        </button>
+                    </div>
+                </form>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-auto">
+                        <li class="nav-item">
+                            <a class="nav-link " href="index">หน้าแรก</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="projectCart" id="navbarLightDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">โครงการ</a>
+
+                            <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
+                                <li><a class="dropdown-item" href="projectCart">รวมโครงการ</a></li>
+
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index">ข่าวสาร</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="contract">ติดต่อ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login">ออกจากระบบ</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
         <section class="trending-podcast-section section-padding ">
             <div class="container">
@@ -49,16 +95,17 @@
                     <div class="col-lg-4 col-12 mb-4 mb-lg-0">
                         <div class="custom-block2 custom-block-full">
                             <div class="custom-block-image-wrap">
-                                <a href="">
-                                    <img src="https://www.ramafoundation.or.th/give/uploads/projects/thumbnail/6253af47601de.jpg"
-                                        class="custom-block-image img-fluid" alt="">
+                                <a href="detail">
+                                    <img src="https://www.ramafoundation.or.th/give/uploads/projects/thumbnail/6253af47601de.jpg" 
+                                    class="custom-block-image img-fluid"
+                                        alt="">
                                 </a>
                             </div>
 
                             <div class="custom-block-info">
                                 <h5 class="mb-2">
-                                    <a href="">
-                                        โครงการศูนย์นวัตกรรมการแพทย์
+                                    <a href="detail">
+                                    โครงการศูนย์นวัตกรรมการแพทย์
                                     </a>
                                 </h5>
 
@@ -70,12 +117,12 @@
                                         <strong>ผู้ก่อตั้งโครงการ</strong>
                                     </p>
                                 </div>
-
-                                <p class="mb-0">ติดตามรายละเอียด</p>
+                                <span class="badge">ของใช้</span>
+                                {{-- <p class="mb-0">ติดตามรายละเอียด</p> --}}
 
                                 <div class="custom-block-bottom d-flex justify-content-between mt-3">
 
-                                    <a href="#" class="bi-heart me-1">
+                                    <a href="detail" class="bi-heart me-1">
                                         <span>2.5k</span>
                                     </a>
 
@@ -88,16 +135,17 @@
                     <div class="col-lg-4 col-12 mb-4 mb-lg-0">
                         <div class="custom-block2 custom-block-full">
                             <div class="custom-block-image-wrap">
-                                <a href="">
-                                    <img src="https://www.ramafoundation.or.th/give/uploads/projects/thumbnail/5faa4894ce207.jpg"
-                                        class="custom-block-image img-fluid" alt="">
+                                <a href="detail">
+                                    <img src="https://www.ramafoundation.or.th/give/uploads/projects/thumbnail/5faa4894ce207.jpg" 
+                                    class="custom-block-image img-fluid"
+                                        alt="">
                                 </a>
                             </div>
 
                             <div class="custom-block-info">
                                 <h5 class="mb-2">
-                                    <a href="">
-                                        โครงการทุนการศึกษารามาธิบดี
+                                    <a href="detail">
+                                    โครงการทุนการศึกษารามาธิบดี
                                     </a>
                                 </h5>
 
@@ -111,12 +159,13 @@
                                         <strong>ผู้ก่อตั้งโครงการ</strong>
                                     </p>
                                 </div>
-
-                                <p class="mb-0">ติดตามรายละเอียด</p>
+                                <span class="badge">ของใช้</span>
+                                <span class="badge">ของเล่น</span>
+                                {{-- <p class="mb-0">ติดตามรายละเอียด</p> --}}
 
                                 <div class="custom-block-bottom d-flex justify-content-between mt-3">
 
-                                    <a href="#" class="bi-heart me-1">
+                                    <a href="detail" class="bi-heart me-1">
                                         <span>2.5k</span>
                                     </a>
 
@@ -129,7 +178,7 @@
                 </div>
             </div>
         </section>
-
+       
     </main>
     <!-- JAVASCRIPT FILES -->
     <script src="js/jquery.min.js"></script>

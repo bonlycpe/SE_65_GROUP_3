1627fcda494da581c77469e41ad0a2af2fefd4b7
NPJA <123374602+NPJA@users.noreply.github.com>
2023-04-01 22:22:47 +0700

    Update Campaign.php

--- myProject/app/Models/Campaign.php
+++ myProject/app/Models/Campaign.php
@@ -11,6 +11,7 @@ class Campaign extends Model
     use HasFactory;
     
     protected $fillable = [
+        'id',
         'Name',
         'Description',
         'Status'
@@ -23,6 +24,27 @@ class Campaign extends Model
         return $managers;
     }
 
-    
+    public static function getAll(){
+        $campaign = DB::select('select * from campaign');
+
+        return $campaign;
+    }
+
+    public static function getMoney(){
+        $campaignMoney = DB::table('campaign')
+        ->join('campaign_money','campaign.Id','=','campaign_money.campaign_money_id')
+        ->select('campaign.Id','campaign_money.Goal','campaign.Name','campaign.Description','campaign.Status','campaign.Image','campaign_money.campaign_money_id')->get();       
+        return $campaignMoney;
+    }
+
+    public static function getObject(){
+        $campaignObject = DB::table('campaign')
+        ->Join('campaign_object', 'campaign.Id', '=', 'campaign_object.campaign_object_id')
+        ->leftJoin('campaign_object as cu', 'campaign.Id', '=', 'campaign_object.Tag')
+        ->select('campaign.Id','campaign.Name','campaign.Description','campaign.Status','campaign.Image','cu.Tag')
+        ->get();       
+
+        return $campaignObject;
+    }
 
 }

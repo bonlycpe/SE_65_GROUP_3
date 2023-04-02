2406ff5de89c5048220ffa8ae7ec29d74f636a36
NPJA <123374602+NPJA@users.noreply.github.com>
2023-04-02 18:55:37 +0700

    Update MoneyCampaign.php

--- myProject/app/Models/MoneyCampaign.php
+++ myProject/app/Models/MoneyCampaign.php
@@ -22,7 +22,7 @@ class MoneyCampaign extends Model
     public static function getAll() {
         $campaign = DB::table('campaign_money')
         ->join('campaign','campaign_money_id','=','campaign.Id')
-        ->select('campaign_money.campaign_money_id','campaign_money.Goal','campaign.Name','campaign.Description','campaign.Status','campaign_money.Image as Image','campaign_money.total')
+        ->select('campaign_money.campaign_money_id','campaign_money.Goal','campaign.Name','campaign.Description','campaign.Status','campaign.Image as Image','campaign_money.total')
         ->get();
         return $campaign;
     }
